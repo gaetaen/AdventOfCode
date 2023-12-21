ab830bed35e417bc36544aff3340bb7a1861f49b
gaetaen <68718204+gaetaen@users.noreply.github.com>
2023-12-21 16:29:49 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,33 +8,33 @@ Stopwatch stopwatch = new();
 
 try
 {
-    string day1Content = File.ReadAllText("2023\\Day-1\\Input.txt");
-    string day2Content = File.ReadAllText("2023\\Day-2\\Input.txt");
-    string day3Content = File.ReadAllText("2023\\Day-3\\Input.txt");
-    string day4Content = File.ReadAllText("2023\\Day-4\\Input.txt");
+    string day1Input = File.ReadAllText("2023\\Day-1\\Input.txt");
+    string day2Input = File.ReadAllText("2023\\Day-2\\Input.txt");
+    string day3Input = File.ReadAllText("2023\\Day-3\\Input.txt");
+    string day4Input = File.ReadAllText("2023\\Day-4\\Input.txt");
 
     // Day 1
-    RunDayChallenge("Day 1.1 Trebuchet", () => Trebuchet.Calibration(day1Content, false));
-    RunDayChallenge("Day 1.2 Trebuchet", () => Trebuchet.Calibration(day1Content, true));
+    RunDailyChallenge("Day 1.1 Trebuchet", () => Trebuchet.Calibration(day1Input, false));
+    RunDailyChallenge("Day 1.2 Trebuchet", () => Trebuchet.Calibration(day1Input, true));
 
     // Day 2
-    RunDayChallenge("Day 2.1 CubeConundrum", () => CubeConundrum.GamePossibility(12, 13, 14, day2Content));
-    RunDayChallenge("Day 2.2 CubeConundrum", () => CubeConundrum.CubePower(day2Content));
+    RunDailyChallenge("Day 2.1 CubeConundrum", () => CubeConundrum.GamePossibility(12, 13, 14, day2Input));
+    RunDailyChallenge("Day 2.2 CubeConundrum", () => CubeConundrum.CubePower(day2Input));
 
     // Day 3
-    RunDayChallenge("Day 3.1 Gear Ratios", () => GearRatios.SchematicSum(day3Content));
-    RunDayChallenge("Day 3.2 Gear Ratios", () => GearRatios.SchematicMultiply(day3Content));
+    RunDailyChallenge("Day 3.1 Gear Ratios", () => GearRatios.SchematicSum(day3Input));
+    RunDailyChallenge("Day 3.2 Gear Ratios", () => GearRatios.SchematicMultiply(day3Input));
 
     // Day 4
-    RunDayChallenge("Day 4.1 Scratchcards", () => Scratchcards.CalculatePoints(day4Content));
-    RunDayChallenge("Day 4.2 Scratchcards", () => Scratchcards.TotalScratchcards(day4Content));
+    RunDailyChallenge("Day 4.1 Scratchcards", () => Scratchcards.CalculatePoints(day4Input));
+    RunDailyChallenge("Day 4.2 Scratchcards", () => Scratchcards.TotalScratchcards(day4Input));
 }
 catch (IOException ex)
 {
     Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
 }
 
-void RunDayChallenge(string challengeName, Func<int> challenge)
+void RunDailyChallenge(string challengeName, Func<int> challenge)
 {
     stopwatch.Restart();
     int result = challenge();
